41b817eafa3dae457cdcab29025066103bae4ee2
LucasJavaudin <lucas@lucasjavaudin.com>
2025-10-09 15:47:12 +0200

    Automatic version reading from pyproject

--- src/mobisurvstd/__init__.py
+++ src/mobisurvstd/__init__.py
@@ -1,10 +1,12 @@
+from importlib.metadata import version
+
 from .classes import SurveyDataReader as SurveyDataReader
 from .classes import read_many as read_many
 from .logger import setup as setup
 from .main import bulk_standardize as bulk_standardize
 from .main import standardize as standardize
 
-__version__ = "0.11.0"
+__version__ = version("mobisurvstd")
 
 # Initialize logging.
 setup()

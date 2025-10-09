4bf84ead41a97473dad6bb3968160e457c5d926e
LucasJavaudin <lucas@lucasjavaudin.com>
2025-10-09 15:38:09 +0200

    Update to version 0.11.0

--- src/mobisurvstd/__init__.py
+++ src/mobisurvstd/__init__.py
@@ -4,5 +4,7 @@ from .logger import setup as setup
 from .main import bulk_standardize as bulk_standardize
 from .main import standardize as standardize
 
+__version__ = "0.11.0"
+
 # Initialize logging.
 setup()
